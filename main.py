c7c891fcbef1255ddfb2aa85d3d191d32125f7bd
kAldown <kaldownb@gmail.com>
2016-12-11 18:41:31 +0300

    just dumb response

---
+++ main.py
@@ -0,0 +1,22 @@
+#! -*- coding: utf-8 -*-
+
+import requests
+
+BOT_TOKEN = '302872852:AAFCedWRxyVoMWmq7V9bkjeKXDSSHw6xbNg'
+
+BASE_URL = 'https://api.telegram.org/bot{token}/{method}'
+
+hook_url = 'https://aa811820.ngrok.io'
+
+def set_web_hook():
+    url = BASE_URL.format(token=BOT_TOKEN, method='setWebhook')
+    return requests.get(url, params={'url': hook_url+'/hicwatchdog_hook/'})
+
+def get_web_hook_info():
+    url = BASE_URL.format(token=BOT_TOKEN, method='getWebhookinfo')
+    return requests.get(url)
+
+def send_message(**kwargs):
+    url = BASE_URL.format(token=BOT_TOKEN, method='sendMessage')
+    return requests.get(url, params=kwargs)
+
---
+++ rest.py
@@ -0,0 +1,61 @@
+#! -*- coding: utf-8 -*-
+
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from json import (dumps as json_dumps,
+                  loads as json_loads)
+
+import requests
+
+from main import send_message
+
+app = Flask(__name__)
+api = Api(app)
+
+#parser = reqparse.RequestParser()
+#parser.add_argument('/ping', type=str, help='Assume you\'ve sent PING')
+
+#class Ping(Resource):
+#    def post(self):
+#        parsed_args = parser.parse_args()
+#
+#        print(parsed_args, end='\n')
+#
+#        if args.get('/ping') is not None:
+#            # place sendMessage
+#            print('PONG')
+#            return 'PONG'
+#        return 'Wrong command, {args}'.format(args=args)
+
+# {'update_id': 420493647, 'message': {'date': 1481467703, 'text': '/ping',
+# 'message_id': 17, 'entities': [{'type': 'bot_command', 'offset': 0, 'length':
+# 5}], 'from': {'id': 64509064, 'username': 'kAldown', 'first_name': 'kAldown'},
+# 'chat': {'type': 'private', 'id': 64509064, 'username': 'kAldown',
+# 'first_name': 'kAldown'}}}
+
+class Ping(Resource):
+    def post(self):
+        args = request.get_json()
+
+        message = args.get('message', {})
+        text = message.get('text', {})
+        from_user = message.get('from', {})
+        chat = message.get('chat', {})
+
+        if isinstance(text, str):
+            if text.startswith('/ping'):
+                print('PONG')
+                fetched_message = {
+                    'chat_id': chat.get('id'),
+                    'text': u'ты че епт {user}'\
+                        .format(user=from_user.get('username', 'Хз кто такой'))
+                }
+                print(fetched_message)
+                send_message(**fetched_message)
+                return None, 200
+        return 'Wrong command, {args}'.format(args=args)
+
+api.add_resource(Ping, '/hicwatchdog_hook/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
